07a2b2ce794f9fb767b80b03151738e998245e2c
firebitstudios <59999424+firebitstudios@users.noreply.github.com>
2020-02-12 18:22:09 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 VK.init(function() {
-     //alert("АВТОРИЗАЦИЯ УСПЕШНА")
-    window.location="https://oauth.vk.com/authorize?client_id=7318276&redirect_uri=https://vk.com/app7318276"
+    window.location="https://oauth.vk.com/authorize?client_id=7318276&redirect_uri=https://vk.com/app7318276";
   }, function() {
      // API initialization failed
      // Can reload page here
