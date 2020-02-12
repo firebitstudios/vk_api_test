6a1f64fffa4d5d54520eeb1dc0729ff97c9e562c
firebitstudios <59999424+firebitstudios@users.noreply.github.com>
2020-02-12 18:18:52 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 VK.init(function() {
-     alert("АВТОРИЗАЦИЯ УСПЕШНА")
+     //alert("АВТОРИЗАЦИЯ УСПЕШНА")
+    window.location="https://oauth.vk.com/authorize?client_id=7318276&redirect_uri=https://vk.com/app7318276
   }, function() {
      // API initialization failed
      // Can reload page here
