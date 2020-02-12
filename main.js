a0b9146df79d39b01074148410bf92f027e128fe
firebitstudios <59999424+firebitstudios@users.noreply.github.com>
2020-02-12 19:58:54 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,4 +3,3 @@ VK.init(function() {
   }, function() {
      document.write("FAILED");
 }, '5.103');
-window.location="https://oauth.vk.com/authorize?client_id=7318276&redirect_uri=https://vk.com/app7318276";
