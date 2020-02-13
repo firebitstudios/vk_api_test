6acbccb542bbc07f0fe9fb45c9d9fc26579944c5
firebitstudios <59999424+firebitstudios@users.noreply.github.com>
2020-02-13 18:27:36 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 VK.init(function() {
-    document.write("SUCCESS");
+    document.write("SUCCESS\n");
+    document.write("YOUR ID IS: ");
   }, function() {
      document.write("FAILED");
 }, '5.103');
