8bc82d442662ade1b6383d457e7bbd10acd113f1
firebitstudios <59999424+firebitstudios@users.noreply.github.com>
2020-02-13 19:54:19 +0300

    Update main.js

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ VK.init(function() {
      // Can reload page here
 }, '5.103'); 
 function init(){
-    VK.api("users.get", {"v":"5.103"}, function (data){
-        VKID = data.response[0].id;
+    VK.api("users.get", {"fields":"first_name", "v":"5.103"}, function (data){
+        VKID = data.response[0].first_name;
         document.write(VKID);
     });
 }
